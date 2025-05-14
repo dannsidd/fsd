0d449ffe475604eb14b1baf940f9cf384646c507
dannsidd <danushsiddarth0509@gmail.com>
2025-05-14 20:59:28 +0530

    Initial commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='md:mx-10'>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10  mt-40 text-sm'>
+
+        <div>
+          <img className='mb-5 w-40' src={assets.logo} alt="" />
+          <p className='w-full md:w-2/3 text-gray-600 leading-6'></p>
+        </div>
+
+
+        <div>
+          <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+          <ul className='flex flex-col gap-2 text-gray-600'>
+            <li>0422-23456789</li>
+            <li>DocSched@gmail.com</li>
+          </ul>
+        </div>
+
+      </div>
+
+      <div>
+        <hr />
+        <p className='py-5 text-sm text-center'>Copyright 2025 - All Right Reserved.</p>
+      </div>
+
+    </div>
+  )
+}
+
+export default Footer
